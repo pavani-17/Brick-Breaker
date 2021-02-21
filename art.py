3c5aa818286419804f850de98f653ea1ab76ccc5
Pavani <babburi.pavani@gmail.com>
2021-02-21 15:33:11 +0530

    Done

---
+++ art.py
@@ -0,0 +1,28 @@
+import numpy as np
+from colorama import Fore, Back
+
+def getGameOver(win):
+
+    arr = []
+
+    with open('gameover.txt','r') as f:
+        for line in f:
+            arr.append(list(line.strip('\n')))
+
+    if win == True:
+        with open('win.txt','r') as f:
+            for line in f:
+                arr.append(list(line.strip('\n')))
+
+        color = Fore.GREEN
+
+    else:
+        with open('lose.txt','r') as f:
+            for line in f:
+                arr.append(list(line.strip('\n')))
+
+        color = Fore.RED
+
+    return np.array(arr, dtype=object), color
+
+        
